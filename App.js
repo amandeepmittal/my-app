014d2696bae0506e4a082d8bdc417f4f2dd685af
Aman Mittal <amandeepmittal@live.com>
2024-03-04 16:49:51 +0530

    Make a typo

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        Opn up App.js to start working on your app!
+        Oepn up App.js to start working on your app!
       </Text>
       <StatusBar style="auto" />
     </View>
