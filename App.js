782539f561bc5525fa12cf6be38a53bbd27df134
Aman Mittal <amandeepmittal@live.com>
2024-03-04 17:29:47 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* Let's fix typo on preview branch. We need preview build installed for this on the device. */}
       <Text style={styles.title}>
         Open up App.js to start working on your device!
       </Text>
