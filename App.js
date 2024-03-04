6b2f433a9822eabdb0770e0742ba46af16ebcf2f
Aman Mittal <amandeepmittal@live.com>
2024-03-04 17:27:45 +0530

    Revert to initial commit

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* Let's fix typo on preview branch. We need preview build installed for this on the device. */}
       <Text style={styles.title}>
-        Oepn up App.js to start working on your app!
+        Open up App.js to start working on your device!
       </Text>
       <StatusBar style="auto" />
     </View>
