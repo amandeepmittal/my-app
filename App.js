3ae037eb68f37e3d7ea006c786deefb9f437c9f7
Aman Mittal <amandeepmittal@live.com>
2024-03-06 18:12:17 +0530

    Push update.yml

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        Open up App.js to start working on your device!
+        Oepn up App.js to start working on your device!
       </Text>
       <Text>This is a preview build</Text>
       <StatusBar style="auto" />
