0ba2b109f412f34f95c94af34a65c31af3831247
Aman Mittal <amandeepmittal@live.com>
2024-03-06 18:20:07 +0530

    Fix typo

--- App.js
+++ App.js
@@ -5,7 +5,13 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        Oepn up App.js to start working on your device!
+        {/*
+         * Let's fix typo.
+         * We need development build installed on the device.
+         * We will use GitHub Actions on the "main" branch to push changes and preview them
+         * in a development build.
+         */}
+        Open up App.js to start working on your device!
       </Text>
       <Text>This is a preview build</Text>
       <StatusBar style="auto" />
